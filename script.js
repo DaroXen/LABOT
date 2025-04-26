c9efb4967582a7c4a806e61587daca1fd883c45b
DaroXen <dario.delreal@gmail.com>
2025-04-26 13:07:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { createChat } from 'https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js';
 
 createChat({
-  webhookUrl: https://emeem.app.n8n.cloud/webhook/716458ef-494e-4c03-b04a-93b0868a63ae/chat,
+  webhookUrl: 'https://emeem.app.n8n.cloud/webhook/716458ef-494e-4c03-b04a-93b0868a63ae/chat',
   initialMessages: ['¡Hola! ¿En qué puedo ayudarte?'],
   defaultLanguage: 'es',
   i18n: {
